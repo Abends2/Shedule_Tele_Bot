f39edf3e6c60d0d4a7af1e849ea0b4abfafe7ad5
Abends2 <Ivan-Belaev-2003@yandex.ru>
2022-05-01 14:55:50 +0300

    more

--- modules/xls_scraper.py
+++ modules/xls_scraper.py
@@ -1,11 +1,16 @@
 from openpyxl import load_workbook
 
-book = load_workbook(filename="./files/shedule.xlsx")
+#from bot import data
+
+book = load_workbook(filename="../files/shedule.xlsx")
 sheet = book["Shedule"]
 
+#if data:
 result = ''
-for i in range(1, 27):
-	cell = sheet['A' + str(i)].value
+for i in range(1, 32):
+	cell = sheet['B' + str(i)].value
 	if ";" in cell:
 		cell = cell[0:cell.find(";") + 1] + '\n' + '   ' + cell[cell.find(";") + 2:]
 	result += cell + '\n'
+#else:
+	#print("Err")
