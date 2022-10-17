b7636adf816fbf770a41f60ee5650b084c716a1d
Abends2 <98419421+Abends2@users.noreply.github.com>
2022-10-17 23:34:54 +0300

    fixes

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 # Main object of bot
-bot = Bot(token='5224591766:AAEusyMiqE_lkFESzCS58w9bPCYyMHt8S3s')
+bot = Bot(token='BOT_TOKEN_HERE')
 
 # Dispatcher for bot
 dispatcher = Dispatcher(bot, storage=MemoryStorage())
